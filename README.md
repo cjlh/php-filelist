2c3f65efbcb8c40b356c680b157ad83aa1ee1846
Caleb Hamilton <cjlhamilton@gmail.com>
2017-08-07 22:29:15 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # php-filelist
+
 A barebones PHP script to list the contents of a directory
+
+## How to use
+1. Place index.php in a directory of your choice
+2. That's it! Now visit the directory in your web browser
+
+## Todo
+- [ ] Implement toggle for displaying `index.php` in list
+
+***
+
+[https://github.com/cjlh/php-filelist](https://github.com/cjlh/php-filelist)
---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+	<!--
+		php-filelist
+		author: Caleb Hamilton
+		website: calebh.com
+	-->
+	<head>
+		<?php
+			$url = $_SERVER[HTTP_HOST];
+			$url_text = str_replace("www.", "", $url);
+			$http = "http://";
+			if (!empty($_SERVER[HTTPS])) {
+				$http = "https://";
+			}
+			$dir = str_replace("/", " / ", rtrim($_SERVER[REQUEST_URI], "/"));
+			$title = ltrim($dir, " / ");
+			echo "<title>$title</title>";
+		?>
+		<style>
+		</style>
+	</head>
+	<body>
+		<?php
+			echo "<p><a href=\"$http$url\">$url_text</a>$dir</p>";
+		?>
+		<ul>
+			<?php
+				$dir    = './';
+				$filelist = scandir($dir);
+				for ($i = 2; $i < sizeof($filelist); $i++) {
+					echo "<li><a href=\"./$filelist[$i]\">$filelist[$i]</a></li>\n";
+				}
+			?>
+		</ul>
+	</body>
+</html>
